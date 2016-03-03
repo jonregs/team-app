347a256961ea64e8ce48852c6634d5f72984791b
Jonathan Jude Regalado <jonegalado@Jonathans-MacBook-Pro.local>
2016-03-02 20:23:00 -0800

    Initial commit

---
+++ app/src/main/java/com/rega/teamup/NearbyGamesFragment.java
@@ -0,0 +1,21 @@
+package com.rega.teamup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Jonathan on 2/17/2016.
+ */
+public class NearbyGamesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.nearby_games_fragment_layout,null);
+    }
+}
