90a9bc164ca8f7fcc7a28296454c12a258cf357d
Jonathan Jude Regalado <jonegalado@Jonathans-MacBook-Pro.local>
2016-03-04 19:52:15 -0800

    splash screen not working

---
+++ app/src/main/java/com/rega/teamup/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.rega.teamup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jonegalado on 3/4/16.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
