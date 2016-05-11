c3112ef944eb3cfce5b5c597044ba6e1032dc4d6
Jonathan Jude Regalado <jonegalado@Jonathans-MacBook-Pro.local>
2016-05-10 21:59:52 -0700

    cardview code not working

---
+++ app/src/main/java/com/rega/teamup/Model.java
@@ -0,0 +1,10 @@
+package com.rega.teamup;
+
+/**
+ * Created by jonegalado on 5/10/16.
+ */
+
+public class Model {
+    char name;
+    byte age;
+}
--- app/src/main/java/com/rega/teamup/YourGamesFragment.java
+++ app/src/main/java/com/rega/teamup/YourGamesFragment.java
@@ -3,9 +3,14 @@ package com.rega.teamup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,11 +18,60 @@ import android.view.ViewGroup;
  */
 public class YourGamesFragment extends Fragment {
 
+
+    RecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
+    private static List<Model> demoData;
+
+    public YourGamesFragment() {
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.your_games_fragment_layout,null);
+        View rootView = inflater.inflate(R.layout.your_games_fragment_layout, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.myList);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        demoData = new ArrayList<Model>();
+        char c = 'A';
+        for (byte i = 0; i < 20; i++) {
+            Model model = new Model();
+            model.name = c++;
+            model.age = (byte) (20 + 1);
+            demoData.add(model);
+        }
+        adapter = new RecyclerViewAdapter(demoData);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
     }
+
+
+    /*@Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView = (RecyclerView) getView().findViewById(R.id.myList);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        demoData = new ArrayList<Model>();
+        char c = 'A';
+        for (byte i = 0; i < 20; i++) {
+            Model model = new Model();
+            model.name = c++;
+            model.age = (byte) (20 + 1);
+            demoData.add(model);
+        }
+        adapter = new RecyclerViewAdapter(demoData);
+        recyclerView.setAdapter(adapter);
+    }*/
 
     public void setArguments() {
     }
