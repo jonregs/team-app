65fe50b3e925582bd8440bbf8f014b19643da62f
Jon <regsjondev@gmail.com>
2016-06-21 20:21:53 -0700

    listview in fragments working

--- app/src/main/java/com/rega/teamup/YourGamesFragment.java
+++ app/src/main/java/com/rega/teamup/YourGamesFragment.java
@@ -3,22 +3,40 @@ package com.rega.teamup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.rega.teamup.data.Flower;
+import com.rega.teamup.data.FlowerData;
+
+import java.util.List;
 
 
 /**
  * Created by Jonathan on 2/17/2016.
  */
-public class YourGamesFragment extends Fragment {
+public class YourGamesFragment extends ListFragment {
+
+    List<Flower> flowers = new FlowerData().getFlowers();
+
+    public YourGamesFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FlowerArrayAdapter adapter = new FlowerArrayAdapter(getActivity(), R.layout.flower_listitem, flowers);
+        setListAdapter(adapter);
+    }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.your_games_fragment_layout,null);
-    }
-
-    public void setArguments() {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.your_games_fragment_layout, container, false);
+        return rootView;
     }
 }
